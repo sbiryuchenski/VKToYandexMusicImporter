3b59f8ac3a03da7057482471a933a7497234c2d4
Sergey <serjjjoyl@gmail.com>
2022-02-26 23:40:18 +0300

    Add project files.

---
+++ VKToYandexMusicImporter/MainWindow.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace VKToYandexMusicImporter
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.launchBrowserButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // launchBrowserButton
+            // 
+            this.launchBrowserButton.Location = new System.Drawing.Point(229, 260);
+            this.launchBrowserButton.Name = "launchBrowserButton";
+            this.launchBrowserButton.Size = new System.Drawing.Size(75, 23);
+            this.launchBrowserButton.TabIndex = 0;
+            this.launchBrowserButton.Text = "button1";
+            this.launchBrowserButton.UseVisualStyleBackColor = true;
+            this.launchBrowserButton.Click += new System.EventHandler(this.launchBrowserButton_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 338);
+            this.Controls.Add(this.launchBrowserButton);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button launchBrowserButton;
+    }
+}
+
---
+++ VKToYandexMusicImporter/MainWindow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using System.Diagnostics;
+using System.IO;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Configuration;
+
+namespace VKToYandexMusicImporter
+{
+    public partial class MainWindow : Form
+    {
+        IWebDriver driver;
+        By music;
+        By findFriendMusic;
+        By foundFriend;
+        By countMusic;
+        By song;
+
+        public MainWindow()
+        {
+            music = By.XPath("//li[@id='l_aud']/a");
+            findFriendMusic = By.XPath("//input[@class='ui_search_field _field'][not(contains(@id, 'audio'))]");
+            foundFriend = By.XPath("//a[@class='audio_friend_name fl_l']");
+            countMusic = By.XPath("//div[@class='audio_friend_audios_count fl_l']");
+            song = By.CssSelector("a.audio_row__title_inner _audio_row__title_inner");
+
+            InitializeComponent();
+        }
+        private void InitBrowser()
+        {
+            driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            driver.Url = "https://vk.com/";
+            driver.SwitchTo().Window(driver.CurrentWindowHandle);
+            driver.Manage().Window.Maximize();
+        }
+
+        private void launchBrowserButton_Click(object sender, EventArgs e)
+        {
+            InitBrowser();
+            var result = MessageBox.Show("Авторизуйтесь в ВК и нажмите ОК");
+            if (result == DialogResult.OK)
+            {
+                driver.FindElement(music).Click();
+                driver.FindElement(findFriendMusic).SendKeys("Olga Biryuchinskaya");
+
+                var musicCount = driver.FindElement(countMusic).Text;
+                int numberMusic;
+                int.TryParse(string.Join("", musicCount.Where(c => char.IsDigit(c))), out numberMusic);
+
+                driver.FindElement(foundFriend).Click();
+                if(driver.FindElements(song).Count() != numberMusic)
+                {
+                    driver.FindElements(song).Last().SendKeys(OpenQA.Selenium.Keys.PageDown);
+                }
+            }
+        }
+    }
+}
